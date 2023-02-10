0558ae09332b1475c4d206c7b914e664a383bb17
Oksana Moskviak <84397218+okqsna@users.noreply.github.com>
2023-02-10 23:29:42 +0200

    Add files via upload

--- src/Routes/Episodes/Episodes.js
+++ src/Routes/Episodes/Episodes.js
@@ -2,26 +2,28 @@ import './Episodes.css';
 import Header from '../../Components/Header/Header';
 import PaginationPages from '../../Components/PaginationPages/PaginationPages';
 import React, {useState, useEffect} from 'react';
-
+import FilterEpisodes from '../../Components/FilterEpisodes/FilterEpisodes';
 const Episodes=()=>{
     const [page, setPage]=useState(1);
     const [receivedData, setReceivedData]=useState([]);
     const [loading, setLoading]=useState(true);
+    const [epiName, setEpiName]=useState('');
     const {info, results}=receivedData;
     useEffect(()=>{
-    fetch(`https://rickandmortyapi.com/api/episode/?page=${page}`)
+    fetch(`https://rickandmortyapi.com/api/episode/?page=${page}&name=${epiName}`)
     .then(response=>response.json())
     .then(data=>{
       setReceivedData(data);
       setLoading(false);
       console.log(data);
     })
-    }, [page])
+    }, [page, epiName])
     return(
         <div className="Episodes">
             <Header/>
             <div className='content__Episodes'>
             <h3>Episodes</h3>
+            <FilterEpisodes epiName={epiName} setEpiName={setEpiName}/>
             { loading ? <div className="loading__Chars">
                 <img src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif" width="40px" height='40px' alt="loading"></img>
             </div> :
