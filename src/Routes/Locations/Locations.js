97c032db8cec83920b70d72e3e165dfc0f862424
Oksana Moskviak <84397218+okqsna@users.noreply.github.com>
2023-02-10 23:30:21 +0200

    bug fixed

--- src/Routes/Locations/Locations.js
+++ src/Routes/Locations/Locations.js
@@ -7,25 +7,35 @@ const Locations=()=>{
     const [page, setPage]=useState(1);
     const [typeloc, setTypeloc]=useState('');
     const [dimension, setDimension]=useState('');
+    const [name, setName]=useState('');
     const [receivedData, setReceivedData]=useState([]);
     const [loading, setLoading]=useState(true);
+    const [error, setError]=useState(null);
     const {info, results}=receivedData;
     useEffect(()=>{
-    fetch(`https://rickandmortyapi.com/api/location/?page=${page}&type=${typeloc}&dimension=${dimension}`)
+    fetch(`https://rickandmortyapi.com/api/location/?page=${page}&type=${typeloc}&dimension=${dimension}&name=${name}`)
     .then(response=>response.json())
     .then(data=>{
       setReceivedData(data);
       setLoading(false);
-      console.log(data);
     })
-    }, [page, typeloc, dimension])
+    .catch((err)=>{
+        setError(err)
+    })
+    }, [page, typeloc, dimension, name])
     return(
         <div className='Locations'>
              <Header/>
             <div className='content__Locations'>
             <h3>Locations</h3>
-            <FiltersLocation typeloc={typeloc} setTypeloc={setTypeloc} dimension={dimension} setDimension={setDimension}/>
-            { loading ? <div className="loading__Chars">
+            <FiltersLocation typeloc={typeloc} setTypeloc={setTypeloc} dimension={dimension} setDimension={setDimension} name={name} setName={setName}/>
+            {error ?
+            <div className="err__Loc">
+                <h3>404</h3>
+                <p>There is no location with these characteristics</p>
+            </div>:
+            <>
+              { loading ? <div className="loading__Chars">
                 <img src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif" width="40px" height='40px' alt="loading"></img>
             </div> :
             <>
@@ -50,6 +60,9 @@ const Locations=()=>{
             </>
            
         }
+            </>
+            }
+           
          </div>
         </div>
     )
